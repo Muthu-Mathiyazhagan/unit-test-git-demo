b756763cb74328fd77b0dc3e50a2db1e961248f9
Sailochana <sailochana@divum.in>
2022-08-31 15:23:17 +0530

    Function added

--- lib.js
+++ lib.js
@@ -89,3 +89,27 @@ module.exports.whole=function(i){
         return "Not a whole number";
     }
 }
+//90498
+module.exports.primeornot=function(n)
+{
+
+  if (n==1)
+  {
+    return false;
+  }
+  else if(n == 2)
+  {
+    return true;
+  }
+  else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x == 0)
+      {
+        return false;
+      }
+    }
+    return true;  
+  }
+}
